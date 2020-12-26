8123478773d62b81b3b2af26d9d4d24e7ab3a495
Huang Renjie <rhsnake@gmail.com>
2020-12-27 06:15:51 +0800

    Added Task 5

---
+++ CSCAssignment/Task5/Controller/ImageController.cs
@@ -0,0 +1,120 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace CSCAssignment.Task5.Controller
+{
+    public class ImageController : ApiController
+    {
+        [HttpPost]
+        [Route("api/t5/upload")]
+        public async Task<HttpResponseMessage> PostFormData()
+        {
+
+            string aws_id = "";
+            string aws_key = "";
+            string aws_token = "";
+            SessionAWSCredentials aws_cred = new  SessionAWSCredentials(aws_id, aws_key, aws_token);
+            string bitly_token = "";
+
+            RegionEndpoint bucketRegion = RegionEndpoint.USEast1;
+
+            // Check if the request contains multipart/form-data.
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            var provider = new MultipartMemoryStreamProvider();
+
+            try
+            {
+                // Read the form data.
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                var formDatas = new List<byte[]>();
+
+                // Scan the Multiple Parts 
+                foreach (HttpContent contentPart in provider.Contents)
+                {
+                    formDatas.Add(await contentPart.ReadAsByteArrayAsync());
+                }
+
+                byte[] photoByte = formDatas[0];
+
+                MemoryStream photoStream = new MemoryStream();
+                photoStream.Write(photoByte, 0, photoByte.Length);
+
+                string S3PhotoKey = Guid.NewGuid().ToString();
+                string S3BucketName = "task5photobucket";
+
+                IAmazonS3 s3Client = new AmazonS3Client(aws_cred, bucketRegion);
+
+                TransferUtility fileTransferUtility = new TransferUtility(s3Client);
+
+                fileTransferUtility.Upload(photoStream, S3BucketName, S3PhotoKey);
+
+                string itemUrl = String.Format("https://{0}.s3.{1}.amazonaws.com/{2}", S3BucketName, bucketRegion.SystemName, S3PhotoKey);
+
+                UriBuilder url = new UriBuilder();
+                url.Scheme = "https";
+
+                url.Host = "api-ssl.bitly.com";
+                url.Path = "v4/shorten";
+
+                //Make a HTTP request to the NEA Weather service
+                JsonDocument wsResponse = BitlyMakeRequest(url.ToString(), bitly_token, itemUrl);
+                JsonElement json = wsResponse.RootElement;
+
+                string shortenedUrl = json.GetProperty("link").GetString();
+
+                return Request.CreateResponse(HttpStatusCode.OK, new { url = shortenedUrl });
+            }
+            catch (System.Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        public static JsonDocument BitlyMakeRequest(string requestUrl, string token, string itemUrl)
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(requestUrl) as HttpWebRequest;
+                request.Timeout = 15 * 1000;
+                request.KeepAlive = false;
+                request.Method = "POST";
+                request.ContentType = "application/json";
+                request.Headers.Add("Authorization", "Bearer " + token);
+                
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                {
+                    var data = new { long_url = itemUrl};
+                    string jsonStr = JsonSerializer.Serialize(data);
+
+                    streamWriter.Write(jsonStr);
+                }
+
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                JsonDocument jsonDoc = JsonDocument.Parse(response.GetResponseStream());
+                return jsonDoc;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
