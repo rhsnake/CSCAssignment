ed511539f7469790e2ebc59b20e682cbdd62c8db
Huang Renjie <rhsnake@gmail.com>
2021-01-07 21:37:14 +0800

    Completed Task 4

---
+++ CSCAssignment/Task4/Controller/TalentController.cs
@@ -0,0 +1,35 @@
+﻿using CSCAssignment.Task4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CSCAssignment.Task4.Controller
+{
+    public class TalentController : ApiController
+    {
+        static readonly TalentRepository repository = new TalentRepository();
+
+        //[EnableCors(origins: "*", headers: "*", methods: "*")]
+        [Route("api/talents")]
+        public IEnumerable<Talent> GetAllTalents()
+        {
+            return repository.GetAll();
+        }
+
+        [Route("api/talents/{id:int}")]
+        public Talent GetTalent(int id)
+        {
+            Talent item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return item;
+        }
+
+
+    }
+}
